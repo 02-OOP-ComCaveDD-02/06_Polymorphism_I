9ae7008e1a090ea1dd5baf8b4a653ee99cf00a83
Peter <schmidt.multimedia@gmail.com>
2024-07-19 12:30:31 +0200

    05 / Instanziierung der Superklasse!

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -12,6 +12,11 @@ public class App {
         pollObj(worker);
         pollObj(drone);
 
+        //-- Das sollte eigentlich nicht gehen!--//
+        HoneyBee bee = new HoneyBee(); // SuperKlasse!
+        pollObj(bee);
+
+
     }
 
     // Polymorphie
