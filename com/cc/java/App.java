bcb8f10e3ad81bd1ff6cd624cdd7a876ecc4ebdc
Peter <schmidt.multimedia@gmail.com>
2024-07-19 12:06:27 +0200

    03 / 3. Iteration Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,21 +8,41 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.doYourJob());
-        output(queen.fly());
-        output("----------------");
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
 
-        output(worker.doYourJob());
-        output(worker.fly());
-        output("----------------");
-
-        output(drone.doYourJob());
-        output(drone.fly());
-        output("----------------");
 
     }
 
-   
+    // Overloading
+    private static void pollObj(Queen obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
+    }
+
+    // Overloading
+    private static void pollObj(Worker obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
+    }
+
+    // Overloading
+    private static void pollObj(Drone obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
+    }
+
+
+
+
+
+
+
+
    
     private static void output(String outputStr) {
         System.out.println(outputStr);
